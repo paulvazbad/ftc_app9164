497ebd0b4bdb65d9bb053cfe37ba3a9fe0a89670
Paul Vazquez <pev@live.com.mx>
2016-02-23 19:03:02 -0800

    Create Teleop.java
    La version del 23 de febrero.

---
+++ Teleop.java
@@ -0,0 +1,137 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+/**
+ * Created by pev on 10/31/2015.
+ */
+// Version 1.20
+public class TeleOp extends OpMode {
+    DcMotor MotorRightDrive;
+    DcMotor MotorLeftDrive;
+    DcMotor MotorRightDriveCenter;
+    DcMotor MotorLeftDriveCenter;
+    DcMotor BrazoDerecho;
+    DcMotor BrazoDerechoUp;
+    DcMotor BrazoIzquierdo;
+    DcMotor BrazoIzquierdoUp;
+    Servo LeftTrigger;
+    Servo RightTrigger;
+    Servo Defense;
+    Servo Climbers;
+    public void init() {
+
+        MotorRightDrive = hardwareMap.dcMotor.get("right.drive");
+        MotorLeftDrive = hardwareMap.dcMotor.get("left.drive");
+        MotorRightDriveCenter = hardwareMap.dcMotor.get("right.center");
+        MotorLeftDriveCenter = hardwareMap.dcMotor.get("left.center");
+        MotorLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        MotorLeftDriveCenter.setDirection(DcMotor.Direction.REVERSE);
+
+        BrazoDerecho = hardwareMap.dcMotor.get("brazo.derecho");
+        BrazoDerechoUp = hardwareMap.dcMotor.get("brazo.derecho.up");
+
+
+        BrazoIzquierdo = hardwareMap.dcMotor.get("brazo.izquierdo");
+        BrazoIzquierdoUp = hardwareMap.dcMotor.get("brazo.izquierdo.up");
+        BrazoIzquierdoUp.setDirection(DcMotor.Direction.REVERSE);
+        BrazoIzquierdo.setDirection(DcMotor.Direction.REVERSE);
+
+        LeftTrigger = hardwareMap.servo.get("left.trigger");
+        RightTrigger = hardwareMap.servo.get("right.trigger");
+        LeftTrigger.setDirection(Servo.Direction.REVERSE);
+        Climbers = hardwareMap.servo.get("climbers");
+        Defense = hardwareMap.servo.get("protector");
+
+    }
+
+    @Override
+    public void start(){
+        Defense.setPosition(0);
+
+
+
+    }
+    public void loop() {
+        double valor = gamepad1.right_trigger;
+        //Driver1
+        if (valor>0) {
+            MotorLeftDrive.setPower(-gamepad1.right_stick_y);
+            MotorRightDrive.setPower(-gamepad1.left_stick_y);
+            MotorLeftDriveCenter.setPower(-gamepad1.right_stick_y);
+            MotorRightDriveCenter.setPower(-gamepad1.left_stick_y);
+            Defense.setPosition(1);
+
+        }
+        else {
+            MotorRightDriveCenter.setPower(gamepad1.right_stick_y);
+            MotorLeftDriveCenter.setPower(gamepad1.left_stick_y);
+            MotorRightDrive.setPower(gamepad1.right_stick_y);
+            MotorLeftDrive.setPower(gamepad1.left_stick_y);
+            Defense.setPosition(0.5);
+
+        }
+        //LeftTrigger
+        if (gamepad1.left_bumper){
+            LeftTrigger.setPosition(0.5);
+
+        }
+        else{
+            LeftTrigger.setPosition(0);
+
+        }
+        //RightTrigger
+        if(gamepad1.right_bumper){
+            RightTrigger.setPosition(0.5);
+        }
+        else{
+            RightTrigger.setPosition(0);
+        }
+        //Driver2
+        //Brazo Derecho
+        BrazoDerecho.setPower(gamepad2.right_stick_y);
+        BrazoDerechoUp.setPower(gamepad2.right_trigger);
+        if (gamepad2.right_bumper) {
+
+            BrazoDerechoUp.setPower(-.9);
+
+        }
+
+
+
+
+        //BrazoIzquierdo
+        BrazoIzquierdo.setPower(gamepad2.left_stick_y);
+        BrazoIzquierdoUp.setPower(gamepad2.left_trigger);
+
+        if (gamepad2.left_bumper){
+            BrazoIzquierdoUp.setPower(-.9);
+
+        }
+        if (gamepad2.y){
+
+            Climbers.setPosition(1);
+        }
+        else{
+
+            Climbers.setPosition(0.05);
+
+        }
+
+
+
+        telemetry.addData("Game pad value", gamepad2.right_trigger);
+
+
+    }
+
+
+
+
+}
+
+
+
